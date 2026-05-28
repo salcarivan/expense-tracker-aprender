9ea881c9eaaa36ff2e5a05192a1f57f1ae97d8a5
Iván <salarivan97@gmail.com>
2026-05-28 11:00:46 +0200

    Temina de crear el Schema

--- src/models/Expense.js
+++ src/models/Expense.js
@@ -2,16 +2,41 @@ const mongoose =require ('moongose');
 
 const expenseShema = new mongoose.Shema (
     {
-        title: {
-            type: String,
-            required: [true, 'El título es obligatorio'],
-            trim: true,
-            maxlength: [100, 'El título no puede superar 100 carcteres']
-        },
-        amount: {
-            type: Number,
-            required: [true, 'El monto es obligatorio'],
-            min: [0.01, 'El monto debe ser mayor a 0']
-        },
+      title:{
+        type:String,
+        required: [true, 'El título es obligatorio'],
+        trim: true,
+        maxlength: [100, 'El titulo no puede tener más de 100 caracteres']
+      },
+      amount:{
+        type: Number,
+        required: [true, 'El monto es obligatorio'],
+        min: [0.01, 'El monto debe ser superior a 0']
+      },
+      category:{
+        type: [String],
+        required: [true, 'La categoría es obligatoria'],
+        enum:{
+            values: ['comida', 'transporte', 'ocio' , 'otros'],
+            default: 'otros',
+            message: 'Categoría inválida',
+        }
+      },
+      date:{
+        type: Date,
+        required: [true, 'La fecha es obligatoria'],
+        default: Date.now
+      },
+      notes:{
+        type: String,
+        trim: true,
+        maxlength: [500, 'Las notas no pueden superar los 500 caracteres'],
+      }
+    },
+    {
+        timestamps: true
     }
 );
+const Expense = mongoose.model('Expense', expenseShema);
+
+module.exprorts = Expense;
