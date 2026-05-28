e5812fc081569943ddf522efb20713f46c60b614
Iván <salarivan97@gmail.com>
2026-05-28 12:10:22 +0200

    Define rutas

---
+++ src/routes/expenseRoutes.js
@@ -0,0 +1,18 @@
+const express = require ('express');
+const router = express.Router();
+
+const {
+    getAllExpenses,
+    getExpenseById,
+    createExpense,
+    updateExpense,
+    deleteExpense
+} = require('../controllers/expenseController');
+
+router.get ('/', getAllExpenses);
+router.get('/:id', getExpenseById);
+router.post('/', createExpense);
+router.put('/:id', updateExpense);
+router.delete('/:id', deleteExpense);
+
+module.exports=router;
