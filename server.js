9bcffd5d9fba5e841ab83329305c42b06c484d81
Iván <salarivan97@gmail.com>
2026-05-28 13:23:26 +0200

    cambios en el servidor

--- server.js
+++ server.js
@@ -5,15 +5,22 @@ const app = require('./src/app');
 const PORT = process.env.PORT || 3000;
 const DB_URI = process.env.MONGODB_URI;
 
-mongoose
-    .connect(DB_URI)
-    .then(() => {
-        console.log('Conectado a MongoDB');
-        app.listen(PORT, () => {
-            console.log(`Servidor corriendo en http://localhost:${PORT}`);
+if (DB_URI) {
+    mongoose
+        .connect(DB_URI)
+        .then(() => {
+            console.log('Conectado a MongoDB');
+            app.listen(PORT, () => {
+                console.log(`Servidor corriendo en http://localhost:${PORT}`);
+            });
+        })
+        .catch((error) => {
+            console.error('Error al conectar a MongoDB: ', error.message);
+            process.exit(1);
         });
-    })
-    .catch((error) => {
-        console.error('Error al conectar a MongoDB: ', error.message);
-        process.exit(1);
+} else {
+    console.warn('MONGODB_URI no está definido. Iniciando servidor sin conexión a la base de datos.');
+    app.listen(PORT, () => {
+        console.log(`Servidor corriendo en http://localhost:${PORT}`);
     });
+}
